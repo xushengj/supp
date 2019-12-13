65d6cd5bdc725d0b11886387f3602f7c74ab2490
Shengjie(John) Xu <shengjie.xu@mail.utoronto.ca>
2019-12-13 00:00:37 -0500

    Added GitHub Actions CI/CD test

--- ui/main.cpp
+++ ui/main.cpp
@@ -3,19 +3,28 @@
 
 #include <QApplication>
 #include <QCoreApplication>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setOrganizationName( "SUPP Development Team" );
     QCoreApplication::setApplicationName( "supp" );
-#if 0
-    testerEntry();
-    return 0;
-#else
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
+
+    QCommandLineParser parser;
+    QCommandLineOption testOption(QStringList() << "t" << "test", QCoreApplication::tr("Run self test"));
+    parser.addOption(testOption);
+    parser.process(a);
+
+    bool isTest = parser.isSet(testOption);
+    if(isTest){
+        // any test (assertion) failure will trigger abort
+        testerEntry();
+        return 0;
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
-#endif
 }
